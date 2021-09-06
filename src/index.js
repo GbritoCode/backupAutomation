0f69b328fa07be7b7923d485fef2c57f4d4a8dd5
Gabriel <gabrielcabeca26@gmail.com>
2021-09-05 21:38:55 -0300

    testing cron

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 // const scriptPath = path.resolve(__dirname, process.env.BACKUP_SCRIPT )
 
-const bat = require.resolve('/home/deploy/backupAutomation/backups/_backupScript.sh');
+const bat = require.resolve(process.env.BACKUP_SCRIPT);
 // const bat1 = require.resolve('../backups/_backupScript.bat');
 
 const app = express();
@@ -39,68 +39,68 @@ const main = async () => {
     throw 'erro';
   }
 
-  //   const generateRawMailData = (message) => {
-  //     const mailOptions = {
-  //       from: message.fromEmail,
-  //       to: message.to,
-  //       cc: message.cc,
-  //       bcc: message.bcc,
-  //       subject: message.subject,
-  //       text: message.bodyTxt,
-  //       html: message.bodyHtml,
-  //       attachments: { filename: message.attachments.name, path: message.attachments.data, encoding: 'base64' },
-  //     };
-  //     return new MailComposer(mailOptions).compile().build();
-  //   };
+  const generateRawMailData = (message) => {
+    const mailOptions = {
+      from: message.fromEmail,
+      to: message.to,
+      cc: message.cc,
+      bcc: message.bcc,
+      subject: message.subject,
+      text: message.bodyTxt,
+      html: message.bodyHtml,
+      attachments: { filename: message.attachments.name, path: message.attachments.data, encoding: 'base64' },
+    };
+    return new MailComposer(mailOptions).compile().build();
+  };
 
-  //   const exampleSendEmail = async () => {
-  //     const message = {
-  //       fromEmail: 'suporte@tovoit.com.br',
-  //       to: ['gabrielcabeca26@gmail.com'],
-  //       cc: [],
-  //       bcc: [],
-  //       subject: 'Alteração de senha',
-  //       bodyTxt: '',
-  //       bodyHtml: `Olá <strong>hjkh</strong> <br> Foi solicitado uma alteração de senha para o seu usuário no aplicativo Tovo,<br>
-  //       caso essa alteração não tenha sido solicitada por você, altere sua senha e entre em contato com o admnistrador<br>
-  //       segue sua nova senha, é aconselhável alterá-la ao fazer login no sistema.<br>
-  //       <strong style="margin-left: 25%" > hjkhjk<strong>
-  //       `,
-  //       attachments: {
-  //         name: `backup_${date}`,
-  //         data: file,
-  //       },
-  //     };
-  //     const ses = new AWS.SESV2(sesConfig);
-  //     const params = {
-  //       Content: { Raw: { Data: await generateRawMailData(message) } },
-  //       Destination: {
-  //         ToAddresses: message.to,
-  //         BccAddresses: message.bcc,
-  //         CcAddresses: message.cc,
-  //       },
-  //       FromEmailAddress: message.fromEmail,
-  //       ReplyToAddresses: message.replyTo,
-  //     };
-  //     return ses.sendEmail(params).promise();
-  //   };
-  //   try {
-  // const response = await exampleSendEmail();
+  const exampleSendEmail = async () => {
+    const message = {
+      fromEmail: 'suporte@tovoit.com.br',
+      to: ['gabrielcabeca26@gmail.com'],
+      cc: [],
+      bcc: [],
+      subject: 'Alteração de senha',
+      bodyTxt: '',
+      bodyHtml: `Olá <strong>hjkh</strong> <br> Foi solicitado uma alteração de senha para o seu usuário no aplicativo Tovo,<br>
+        caso essa alteração não tenha sido solicitada por você, altere sua senha e entre em contato com o admnistrador<br>
+        segue sua nova senha, é aconselhável alterá-la ao fazer login no sistema.<br>
+        <strong style="margin-left: 25%" > hjkhjk<strong>
+        `,
+      attachments: {
+        name: `backup_${date}`,
+        data: file,
+      },
+    };
+    const ses = new AWS.SESV2(sesConfig);
+    const params = {
+      Content: { Raw: { Data: await generateRawMailData(message) } },
+      Destination: {
+        ToAddresses: message.to,
+        BccAddresses: message.bcc,
+        CcAddresses: message.cc,
+      },
+      FromEmailAddress: message.fromEmail,
+      ReplyToAddresses: message.replyTo,
+    };
+    return ses.sendEmail(params).promise();
+  };
   try {
-    rmSync(file);
+    const response = await exampleSendEmail();
+    try {
+      rmSync(file);
+    } catch (err) {
+      console.error(err);
+      throw new Error(err);
+    }
+    console.log(response);
   } catch (err) {
-    console.error(err);
+    console.log(err.message);
     throw new Error(err);
   }
-  // console.log(response);
-//   } catch (err) {
-//     console.log(err.message);
-//     throw new Error(err);
-//   }
 };
 
 const backup = async () => {
-  const proc = spawnSync('/home/deploy/backupAutomation/backups/_backupScript.sh', [date]);
+  const proc = spawnSync(bat, [date]);
   console.log('--------------');
   console.log(proc);
   console.log(proc.output.toString('utf-8'));
