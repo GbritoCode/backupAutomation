2f62214ff130013a7e295c6c624c0704035b4808
Gabriel <gabrielcabeca26@gmail.com>
2021-09-05 21:27:24 -0300

    testing cron

--- src/index.js
+++ src/index.js
@@ -39,60 +39,60 @@ const main = async () => {
     throw 'erro';
   }
 
-  const generateRawMailData = (message) => {
-    const mailOptions = {
-      from: message.fromEmail,
-      to: message.to,
-      cc: message.cc,
-      bcc: message.bcc,
-      subject: message.subject,
-      text: message.bodyTxt,
-      html: message.bodyHtml,
-      attachments: { filename: message.attachments.name, path: message.attachments.data, encoding: 'base64' },
-    };
-    return new MailComposer(mailOptions).compile().build();
-  };
+  //   const generateRawMailData = (message) => {
+  //     const mailOptions = {
+  //       from: message.fromEmail,
+  //       to: message.to,
+  //       cc: message.cc,
+  //       bcc: message.bcc,
+  //       subject: message.subject,
+  //       text: message.bodyTxt,
+  //       html: message.bodyHtml,
+  //       attachments: { filename: message.attachments.name, path: message.attachments.data, encoding: 'base64' },
+  //     };
+  //     return new MailComposer(mailOptions).compile().build();
+  //   };
 
-  const exampleSendEmail = async () => {
-    const message = {
-      fromEmail: 'suporte@tovoit.com.br',
-      to: ['gabrielcabeca26@gmail.com'],
-      cc: [],
-      bcc: [],
-      subject: 'Alteração de senha',
-      bodyTxt: '',
-      bodyHtml: `Olá <strong>hjkh</strong> <br> Foi solicitado uma alteração de senha para o seu usuário no aplicativo Tovo,<br>
-      caso essa alteração não tenha sido solicitada por você, altere sua senha e entre em contato com o admnistrador<br>
-      segue sua nova senha, é aconselhável alterá-la ao fazer login no sistema.<br>
-      <strong style="margin-left: 25%" > hjkhjk<strong>
-      `,
-      attachments: {
-        name: `backup_${date}`,
-        data: file,
-      },
-    };
-    const ses = new AWS.SESV2(sesConfig);
-    const params = {
-      Content: { Raw: { Data: await generateRawMailData(message) } },
-      Destination: {
-        ToAddresses: message.to,
-        BccAddresses: message.bcc,
-        CcAddresses: message.cc,
-      },
-      FromEmailAddress: message.fromEmail,
-      ReplyToAddresses: message.replyTo,
-    };
-    return ses.sendEmail(params).promise();
-  };
+  //   const exampleSendEmail = async () => {
+  //     const message = {
+  //       fromEmail: 'suporte@tovoit.com.br',
+  //       to: ['gabrielcabeca26@gmail.com'],
+  //       cc: [],
+  //       bcc: [],
+  //       subject: 'Alteração de senha',
+  //       bodyTxt: '',
+  //       bodyHtml: `Olá <strong>hjkh</strong> <br> Foi solicitado uma alteração de senha para o seu usuário no aplicativo Tovo,<br>
+  //       caso essa alteração não tenha sido solicitada por você, altere sua senha e entre em contato com o admnistrador<br>
+  //       segue sua nova senha, é aconselhável alterá-la ao fazer login no sistema.<br>
+  //       <strong style="margin-left: 25%" > hjkhjk<strong>
+  //       `,
+  //       attachments: {
+  //         name: `backup_${date}`,
+  //         data: file,
+  //       },
+  //     };
+  //     const ses = new AWS.SESV2(sesConfig);
+  //     const params = {
+  //       Content: { Raw: { Data: await generateRawMailData(message) } },
+  //       Destination: {
+  //         ToAddresses: message.to,
+  //         BccAddresses: message.bcc,
+  //         CcAddresses: message.cc,
+  //       },
+  //       FromEmailAddress: message.fromEmail,
+  //       ReplyToAddresses: message.replyTo,
+  //     };
+  //     return ses.sendEmail(params).promise();
+  //   };
   try {
-    const response = await exampleSendEmail();
+    // const response = await exampleSendEmail();
     try {
       rmSync(file);
     } catch (err) {
       console.error(err);
       throw new Error(err);
     }
-    console.log(response);
+    // console.log(response);
   } catch (err) {
     console.log(err.message);
     throw new Error(err);
@@ -141,19 +141,19 @@ app.listen(process.env.APP_PORT, async () => {
         console.log(err);
         throw new Error(err);
       });
-    // const promiseMain = new Promise((resolve, reject) => {
-    //   try {
-    //     resolve(main());
-    //   } catch (err) {
-    //     console.log(err);
-    //     throw 'erro';
-    //   }
-    // });
+    const promiseMain = new Promise((resolve, reject) => {
+      try {
+        resolve(main());
+      } catch (err) {
+        console.log(err);
+        throw 'erro';
+      }
+    });
 
-    // await promiseMain.then(
-    //   () => console.log('promiseMain realizada'),
-    // )
-    //   .catch((err) => { throw new Error(err); });
+    await promiseMain.then(
+      () => console.log('promiseMain realizada'),
+    )
+      .catch((err) => { throw new Error(err); });
 
     const promiseKill = new Promise((resolve, reject) => {
       try {
